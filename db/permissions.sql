133ee7c2584013a0c0f352ad8a11759fea6d44f3
Bloodcount <searosss@gmail.com>
2016-03-14 23:02:24 +0200

    feat: added postgres schema support

--- db/permissions.sql
+++ db/permissions.sql
@@ -1,3 +1,6 @@
 CREATE USER my_user;
 SET PASSWORD FOR 'my_user' = PASSWORD('my_pass');
 GRANT SELECT, INSERT, DELETE, UPDATE on urls to my_user;
+
+-- POSTGRES SPECIFIC
+GRANT USAGE on urls_id_seq to my_user;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+
+-- MYSQL
 CREATE TABLE urls (
     id  int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     url_hash TEXT NOT NULL,
@@ -5,3 +7,9 @@ CREATE TABLE urls (
 );
 
 create unique index duplicate_url on urls(url(500));
+
+-- POSTGRES
+
+CREATE TABLE urls (id SERIAL PRIMARY KEY,
+                   url_hash TEXT NOT NULL,
+                   url TEXT NOT NULL);
