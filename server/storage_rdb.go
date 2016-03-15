e146fa2738f7a1919af4c5e0700820fa9e4363b6
Bloodcount <searosss@gmail.com>
2016-03-15 23:18:33 +0200

    format: gofmt

--- server/storage_rdb.go
+++ server/storage_rdb.go
@@ -1,11 +1,11 @@
 package main
 
-import  (
-            "github.com/golang/glog"
-            "database/sql"
-            "os"
-            "strings"
-        )
+import (
+	"database/sql"
+	"github.com/golang/glog"
+	"os"
+	"strings"
+)
 
 /*
 Implementetaion of the storage_interface for relational databases
@@ -16,84 +16,81 @@ var db *sql.DB //This is a connection pool. It must be global so it is visible i
 /*
 Must be called in the main function. It will create the nessecary environment for the storage.
 */
-func InitStorage(){
-    var err error
-    db, err = sql.Open(os.Getenv("DB_CONNECTION_DRIVER"), os.Getenv("DB_CONNECTION_STRING"))
-    if err != nil {
-        glog.Error(err)
-    }
-    if db == nil {
-        glog.Fatal(db)
-    }
+func InitStorage() {
+	var err error
+	db, err = sql.Open(os.Getenv("DB_CONNECTION_DRIVER"), os.Getenv("DB_CONNECTION_STRING"))
+	if err != nil {
+		glog.Error(err)
+	}
+	if db == nil {
+		glog.Fatal(db)
+	}
 }
 
+func GetURLFromStorage(urlHash string) (string, error) {
+	var err error
 
-func GetURLFromStorage(urlHash string) (string, error){
-    var err error
+	tx, err := db.Begin()
+	rows, err := tx.Query(`SELECT url FROM urls WHERE url_hash = $1`, urlHash)
+	if err != nil {
+		glog.Error(err)
+		tx.Rollback()
+		return ``, err
+	}
+	defer rows.Close()
 
-    tx, err := db.Begin()
-    rows, err := tx.Query(`SELECT url FROM urls WHERE url_hash = $1`, urlHash)
-    if err != nil {
-        glog.Error(err)
-        tx.Rollback()
-        return ``, err
-    }
-    defer rows.Close()
+	var url string
+	for rows.Next() {
+		err = rows.Scan(&url)
+		if err != nil {
+			return ``, err
+		}
+	}
 
-    var url string
-    for rows.Next() {
-       err = rows.Scan(&url)
-       if err != nil {
-            return ``, err
-       }
-   }
-
-   tx.Commit()
-   return url, err
+	tx.Commit()
+	return url, err
 }
-
 
 func AddURLToStorage(urlHash string, url string) (string, error) {
-    var err error
+	var err error
 
-    tx, err := db.Begin()
-    _, err = tx.Query(`INSERT INTO urls(url_hash, url) VALUES($1, $2)`, urlHash, url)
+	tx, err := db.Begin()
+	_, err = tx.Query(`INSERT INTO urls(url_hash, url) VALUES($1, $2)`, urlHash, url)
 
-    // Hash already exists
-    if err != nil && strings.ContainsAny(err.Error(), `Error 1062`) {
-        rows, err := tx.Query(`SELECT url_hash FROM urls WHERE url = $1`, url)
-        if err != nil {
-            glog.Error(err)
-            tx.Rollback()
-            return urlHash, err
-        }
-        defer rows.Close()
-        for rows.Next() {
-           err = rows.Scan(&urlHash)
-           if err != nil {
-                return ``, err
-           }
-       }
+	// Hash already exists
+	if err != nil && strings.ContainsAny(err.Error(), `Error 1062`) {
+		rows, err := tx.Query(`SELECT url_hash FROM urls WHERE url = $1`, url)
+		if err != nil {
+			glog.Error(err)
+			tx.Rollback()
+			return urlHash, err
+		}
+		defer rows.Close()
+		for rows.Next() {
+			err = rows.Scan(&urlHash)
+			if err != nil {
+				return ``, err
+			}
+		}
 
-    } else if err != nil { // some exception has occured
-        glog.Error(err)
-        tx.Rollback()
-    }
+	} else if err != nil { // some exception has occured
+		glog.Error(err)
+		tx.Rollback()
+	}
 
-    tx.Commit()
-    return urlHash, nil
+	tx.Commit()
+	return urlHash, nil
 }
 
-func DeleteURL(urlHash string) error{
-    var err error
+func DeleteURL(urlHash string) error {
+	var err error
 
-    tx, err := db.Begin()
-    _, err = tx.Query(`DELETE FROM urls WHERE url_hash = $1`, urlHash)
-    if err != nil {
-        glog.Error(err)
-        tx.Rollback()
-    }
-    tx.Commit()
-    return err
+	tx, err := db.Begin()
+	_, err = tx.Query(`DELETE FROM urls WHERE url_hash = $1`, urlHash)
+	if err != nil {
+		glog.Error(err)
+		tx.Rollback()
+	}
+	tx.Commit()
+	return err
 }
-
