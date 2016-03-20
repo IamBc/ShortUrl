d5f06f3fda5d825cf0321551043151fd2e4d3f32
Bloodcount <searosss@gmail.com>
2016-03-20 23:03:40 +0200

    fix: syntax errors

--- db/permissions.sql
+++ db/permissions.sql
@@ -2,5 +2,4 @@ CREATE USER my_user;
 CREATE USER my_user WITH  PASSWORD 'my_pass';
 GRANT SELECT, INSERT, DELETE, UPDATE on urls to my_user;
 
--- POSTGRES SPECIFIC
 GRANT USAGE on urls_id_seq to my_user;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
 CREATE TABLE urls (id SERIAL PRIMARY KEY,
-                   url_hash UNIQUE TEXT NOT NULL,
+                   url_hash TEXT UNIQUE NOT NULL,
                    url TEXT UNIQUE NOT NULL);
