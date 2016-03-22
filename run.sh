60cf6ff6956863da371a8c1bddc651f00e3e6308
Bloodcount <searosss@gmail.com>
2016-03-22 21:16:21 +0200

    fix: typo

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "Initializing environment..."
 #NOTE: This is example configuration. If you run the install script this configuration will be enough to run ShortUrl.
 # It is **STRONGLY** encouraged for the dbname, user and password to be changed.
 
-export SHORT_URL_FILES_DIR="../web-client" 
+export SHORT_URL_FILES_DIR="./web-client" 
 export SHORT_URL_FILE_PORT="9002"
 export SHORT_URL_API_PORT="9003"
 export DB_CONNECTION_STRING="user=shrt_url password=123 dbname=short_url sslmode=disable"
