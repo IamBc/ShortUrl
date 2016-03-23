2b697b60ec495f544f6fe79e7fd591ac6a66169b
Bloodcount <searosss@gmail.com>
2016-03-23 23:44:54 +0200

    feat: credentials are the same

--- create_db.sh
+++ create_db.sh
@@ -1,3 +1,3 @@
-sudo su postgres -c 'psql -c "CREATE DATABASE test1;"'
-sudo su postgres -c 'psql -d test1  -f ./db/schema.sql'
-sudo su postgres -c 'psql -d test1  -f ./db/permissions.sql'
+sudo su postgres -c 'psql -c "CREATE DATABASE short_url_def;"'
+sudo su postgres -c 'psql -d short_url_def  -f ./db/schema.sql'
+sudo su postgres -c 'psql -d short_url_def  -f ./db/permissions.sql'
