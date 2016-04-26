d478c00c4188c1fa5db5c5aefcf8f152ad6e2055
Bloodcount <searosss@gmail.com>
2016-04-26 23:56:07 +0300

    feat: table where settings can be stored

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
 CREATE TABLE urls (id SERIAL PRIMARY KEY,
                    url_hash TEXT UNIQUE NOT NULL,
                    url TEXT UNIQUE NOT NULL);
+
+CREATE TABLE short_url(id serial primary key, settings text);
+insert into short_url values(1, '');
