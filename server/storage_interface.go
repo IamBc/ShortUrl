cde79de44fa2756f9e26f274bf2dc2ef4bb08612
Bloodcount <searosss@gmail.com>
2016-04-26 23:48:28 +0300

    feat: GetConfigStorage()

--- server/storage_interface.go
+++ server/storage_interface.go
@@ -9,4 +9,5 @@ type storage interface {
 	GetURLFromStorage(urlHash string) (string, error)
 	DeleteURL(urlHash string) error
 	AddURLToStorage(urlHash string, bodyStr string) error
+	GetConfigStorage() (string, error)
 }
